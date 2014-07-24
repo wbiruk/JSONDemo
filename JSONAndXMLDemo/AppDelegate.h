0361b20ee185e19092f992f93c0dda389449e40d
gabrieltheodoropoulos <gabrielth.devel@gmail.com>
2014-07-24 18:40:02 +0300

    Initial Commit

---
+++ JSONAndXMLDemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  JSONAndXMLDemo
+//
+//  Created by Gabriel Theodoropoulos on 24/7/14.
+//  Copyright (c) 2014 Appcoda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ JSONAndXMLDemo/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  JSONAndXMLDemo
+//
+//  Created by Gabriel Theodoropoulos on 24/7/14.
+//  Copyright (c) 2014 Appcoda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
